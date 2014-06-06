616aa429de1ec1a8407c40f3d3ada6e81db53abe
sunaoka <sunaoka@ap-com.co.jp>
2014-06-06 16:29:30 +0900

    add second

--- lib/just-datetime-picker/formtastic-input.rb
+++ lib/just-datetime-picker/formtastic-input.rb
@@ -25,8 +25,18 @@ module Formtastic
             minute_value = "00"
           end
 
+          second_value_raw = builder.object.send("#{method}_time_second")
+          if not second_value_raw.nil?
+            second_value = second_value_raw
+          elsif not combined_value.nil?
+            second_value = combined_value.sec
+          else
+            second_value = "00"
+          end
+
           hour_value   = sprintf("%02d", hour_value)
           minute_value = sprintf("%02d", minute_value)
+          second_value = sprintf("%02d", second_value)
           
           label_html <<
           builder.text_field("#{method}_date", input_html_options.merge({ :class => "just-datetime-picker-field just-datetime-picker-date datepicker", :value => builder.object.send("#{method}_date"), :maxlength => 10, :size => 10 })) <<
@@ -34,6 +44,8 @@ module Formtastic
           builder.text_field("#{method}_time_hour", input_html_options.merge({ :class => "just-datetime-picker-field just-datetime-picker-time just-datetime-picker-time-hour", :value => hour_value, :maxlength => 2, :size => 2 })) <<
           ":" <<
           builder.text_field("#{method}_time_minute", input_html_options.merge({ :class => "just-datetime-picker-field just-datetime-picker-time just-datetime-picker-time-minute", :value => minute_value, :maxlength => 2, :size => 2 }))
+          ":" <<
+          builder.text_field("#{method}_time_second", input_html_options.merge({ :class => "just-datetime-picker-field just-datetime-picker-time just-datetime-picker-time-second", :value => second_value, :maxlength => 2, :size => 2 }))
         end
       end
     end
